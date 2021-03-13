de63f90fe3666d4458a28c9b9f5e4eebb0dfbc68
Christie Robson <christie.robson@gmail.com>
2021-03-13 17:52:47 +0000

    fix: move leaflet styles to index.html to ensure available when map loads

--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
+import {Helmet} from 'react-helmet';
+import { MemoryRouter } from 'react-router';
 import * as testArticles from './mocks/articles.json';
 
 import Map from '../presentation/components/Map';
@@ -10,7 +12,19 @@ export default {
   component: Map,
 } as Meta;
 
-const Template: Story<any> = (args) => <Map {...args} />;
+const Template: Story<any> = (args) => (
+  <>
+    {/* setup for leaflet */}
+    <Helmet>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+      integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+      crossOrigin=""/>
+    </Helmet>
+    <MemoryRouter>
+      <Map {...args} />
+    </MemoryRouter>
+  </>
+);
 
 
 export const WithArticles = Template.bind({});
